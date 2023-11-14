f44f1e7c27e596d30f294b5b2a24805d163eecd0
duydong123123 <94743075+duydong123123@users.noreply.github.com>
2023-11-14 19:59:46 +0700

    03_NguyenHaDuyDong_Tuan8

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
+import Home from './screens/Home';
+import DetailUser from './screens/DetailUser';
+import AddJob from './screens/AddJob';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="DetailUser" component={DetailUser} options={{headerShown:false}} />
+        <Stack.Screen name="AddJob" component={AddJob} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
